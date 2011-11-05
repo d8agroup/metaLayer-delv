b19ead2a91418cba564e50f4f0f43842b47a0ca8
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2011-11-04 22:28:58 -0400

    Added chart colors

--- visualwidgets/piechart/templates/render_piechart.js
+++ visualwidgets/piechart/templates/render_piechart.js
@@ -7,6 +7,7 @@ var chart_{{ id }} = jQuery.jqplot
 	chart_id,
 	[chart_data],
 	{
+		seriesColors: [ "#D80000", "#F06000", "#D89000", "#780000", "F00000" ],
 		seriesDefaults: 
 		{
 			renderer: jQuery.jqplot.PieRenderer,
