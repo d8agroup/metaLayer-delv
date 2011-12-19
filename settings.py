728bd8adfdf3b379716090cb9c750ae25b357398
Matthew Griffiths <mg@metalayer.com>
2011-12-19 21:58:44 +0000

    Added local and development settings

--- settings.py
+++ settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/usr/local/metaLayer-dashboard/dashboard/dashboard.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -153,4 +142,7 @@ LOGGING = {
 
 import socket
 if socket.gethostname() in ['mattgriffiths']:
+    from settings_development import *
+else:
+    #TODO this needs to be changed to support multiple envs
     from settings_development import *
--- settings_mattgriffiths.py
+++ settings_mattgriffiths.py
@@ -1,9 +1,11 @@
 DEBUG = True
 
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/usr/local/metaLayer-dashboard/dashboard/dashboard.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/matt/code/metaLayer/dashboard/dashboard.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -14,8 +16,8 @@ DATABASES = {
 ENDPOINTS = {
     'datapoints':{
         'metalayer_aggregator':{
-            'add_source':'http://localhost/aggregator/sources/add',
-            'remove_source':'http://localhost/aggregator/sources/remove'
+            'add_source':'http://localhost:5000/sources/add',
+            'remove_source':'http://localhost:5000/sources/remove'
         }
     }
 }
