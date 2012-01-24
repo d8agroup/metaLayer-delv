932981439abb13d91e2fd3be296b8df6e88aa028
Matthew Griffiths <mg@metalayer.com>
2012-01-24 14:09:54 +0300

    changed to the dev config

--- settings_development.py
+++ settings_development.py
@@ -14,8 +14,8 @@ DATABASES = {
         'NAME': 'ml_dashboard', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': 27017,                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
