19d17e03a76ff4b345ca9ee6936e281649b4d3f9
Matthew Griffiths <mg@metalayer.com>
2012-01-31 09:58:46 +0300

    More community site work

--- static/js/thecommunity/profile_page_insights_timeline_insight_container.js
+++ static/js/thecommunity/profile_page_insights_timeline_insight_container.js
@@ -21,11 +21,37 @@
         },
         _render_short_summary_for_visualization:function()
         {
-
+            var insight_container = this;
+            var dashboard = insight_container.data('dashboard');
+            var template_data =
+            {
+                insight:dashboard
+            };
+            var insight_html = $.tmpl('short_summary_for_visualization', template_data);
+            insight_container.html(insight_html);
         },
         _render_short_summary_for_data:function()
         {
-
+            var insight_container = this;
+            var dashboard = insight_container.data('dashboard');
+            var data_logos = [];
+            for (var x=0; x<dashboard.collections.length; x++)
+                for (var y=0; y<dashboard.collections[x].data_points.length; y++)
+                    data_logos[data_logos.length] = dashboard.collections[x].data_points[y].image_large;
+            var template_data =
+            {
+                insight:dashboard,
+                insight_images:data_logos
+            };
+            var insight_html = $.tmpl('short_summary_for_data', template_data);
+            insight_container.html(insight_html);
+            insight_container.profile_page_insights_timeline_insight_container('_apply_insight_actions');
+        },
+        _apply_insight_actions:function()
+        {
+            var insight_container = this;
+            var dashboard = insight_container.data('dashboard');
+            insight_container.find('.insight_actions')
         }
     };
 
