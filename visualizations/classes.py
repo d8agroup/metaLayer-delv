9f34db03a0d9a03e36074f54f58673d9d3236ea8
Matthew Griffiths <mg@metalayer.com>
2012-02-06 20:00:13 +0300

    small changes

--- visualizations/classes.py
+++ visualizations/classes.py
@@ -3,7 +3,9 @@ class VisualizationBase(object):
         pass
 
     def generate_search_query_data(self, config, search_configuration):
-        data_queries = [{'name':dd['value']['value'], 'type':'basic_facet'} for dd in config['data_dimensions'] if dd['value']]
+        data_queries = [
+            {'name':dd['value']['value'], 'type':'basic_facet'} for dd in config['data_dimensions'] if dd['value']
+        ]
         return [data_queries]
 
     def render_javascript_based_visualization(self, config, search_results_collection):
