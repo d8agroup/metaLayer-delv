5d9ad7ae10114bb154ec8dcfce9d95c54f54613d
Matthew Griffiths <mg@metalayer.com>
2012-02-26 22:55:45 +0300

    changed 404 exception

--- outputs/views.py
+++ outputs/views.py
@@ -12,7 +12,7 @@ def generate_output(request, url_identifier):
     if not short_url:
         Logger.Error('%s - generate_output - error - url_identifier:%s not found' % (__name__, url_identifier))
         Logger.Info('%s - generate_output - finished' % __name__)
-        return Http404()
+        raise Http404
     dashboard_id = short_url.dashboard_id
     collection_id = short_url.collection_id
     output_id = short_url.output_id
@@ -20,7 +20,7 @@ def generate_output(request, url_identifier):
     if not dashboard:
         Logger.Error('%s - generate_output - error - dashboard_id:%s not found' % (__name__, dashboard_id))
         Logger.Info('%s - generate_output - finished' % __name__)
-        return Http404()
+        raise Http404
     collection = None
     for c in dashboard.collections:
         if c['id'] == collection_id:
@@ -29,7 +29,7 @@ def generate_output(request, url_identifier):
     if not collection:
         Logger.Error('%s - generate_output - error - collection_id:%s not found' % (__name__, collection_id))
         Logger.Info('%s - generate_output - finished' % __name__)
-        return Http404()
+        raise Http404
     output = None
     if 'outputs' in collection:
         for o in collection['outputs']:
@@ -39,7 +39,7 @@ def generate_output(request, url_identifier):
     if not output:
         Logger.Error('%s - generate_output - error - output_id:%s not found' % (__name__, output_id))
         Logger.Info('%s - generate_output - finished' % __name__)
-        return Http404()
+        raise Http404
     sc = SearchController(collection)
     search_results = sc.run_search_and_return_results()
     oc = OutputController(output)
