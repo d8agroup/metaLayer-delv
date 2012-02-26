7c683aefd4da5e9e9784b6e12c08b39f0f5e7bcf
Matthew Griffiths <mg@metalayer.com>
2012-02-26 15:03:55 +0300

    added points to yahoo placemaker Fixed bug in short url service

--- dashboards/views.py
+++ dashboards/views.py
@@ -5,5 +5,5 @@ from dashboards.models import DashboardShortUrl
 
 def redirect_to_dashboard(request, url_id):
     short_url = DashboardShortUrl.Load(url_id)
-    dashboard = DashboardsController.GetDashboardById(short_url['dashboard_id'])
+    dashboard = DashboardsController.GetDashboardById(short_url.dashboard_id)
     return redirect('http://%s/delv/%s/%s' % (settings.SITE_HOST, dashboard['username'], dashboard['id']))
