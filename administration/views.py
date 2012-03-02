1aa0b7e8dfa31947f5a96e7347aa7a17366f72b8
Matthew Griffiths <mg@metalayer.com>
2012-03-02 21:17:39 +0300

    more work on custom admin pages

--- administration/views.py
+++ administration/views.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -13,10 +14,42 @@ def home(request):
 @staff_member_required
 def users(request):
     template_data = _base_template_data(request)
-    users = User.objects.filter(is_staff=False)
-    template_data['registrations_overall_total'] = users.count()
-    template_data['registrations_overall_past_1'] = len([u for u in users if u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-7))])
-    template_data['registrations_overall_past_2'] = len([u for u in users if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-7)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-14))])
-    template_data['registrations_overall_past_3'] = len([u for u in users if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-14)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-21))])
-    template_data['registrations_overall_past_4'] = len([u for u in users if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-21)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-28))])
+    user_objects = User.objects.filter(is_staff=False)
+    users_past_1 = [u for u in user_objects if u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-7))]
+    users_past_2 = [u for u in user_objects if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-7)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-14))]
+    users_past_3 = [u for u in user_objects if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-14)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-21))]
+    users_past_4 = [u for u in user_objects if u.date_joined < (datetime.datetime.now() + datetime.timedelta(days=-21)) and u.date_joined >= (datetime.datetime.now() + datetime.timedelta(days=-28))]
+
+    template_data['registrations_overall_total'] = user_objects.count()
+    template_data['registrations_overall_past_1'] = len(users_past_1)
+    template_data['registrations_overall_past_2'] = len(users_past_2)
+    template_data['registrations_overall_past_3'] = len(users_past_3)
+    template_data['registrations_overall_past_4'] = len(users_past_4)
+
+    template_data['by_registration_status'] = []
+    for status in [('Held', r'[WAITING|DECLINED]'), ('Let in','APPROVED')]:
+        template_data['by_registration_status'].append(
+            {
+                'name':status[0],
+                'total':len([u for u in user_objects if re.search(status[1], u.profile.registration_status)]),
+                'past_1':len([u for u in users_past_1 if re.search(status[1], u.profile.registration_status)]),
+                'past_2':len([u for u in users_past_2 if re.search(status[1], u.profile.registration_status)]),
+                'past_3':len([u for u in users_past_3 if re.search(status[1], u.profile.registration_status)]),
+                'past_4':len([u for u in users_past_4 if re.search(status[1], u.profile.registration_status)]),
+            }
+        )
+
+    template_data['by_registration_type'] = []
+    for registration_type in [('TED Code', r'^TED.*'), ('METALAYER Code', r'^METALAYER.*')]:
+        template_data['by_registration_type'].append(
+            {
+                'name':registration_type[0],
+                'total':len([u for u in user_objects if re.search(registration_type[1], u.profile.registration_code)]),
+                'past_1':len([u for u in users_past_1 if re.search(registration_type[1], u.profile.registration_code)]),
+                'past_2':len([u for u in users_past_2 if re.search(registration_type[1], u.profile.registration_code)]),
+                'past_3':len([u for u in users_past_3 if re.search(registration_type[1], u.profile.registration_code)]),
+                'past_4':len([u for u in users_past_4 if re.search(registration_type[1], u.profile.registration_code)]),
+            }
+        )
+
     return render_to_response('administration/users.html', template_data, context_instance=RequestContext(request))
